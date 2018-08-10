45ee1b8ee09b8184d102098daac6e805880a551b
alexokeeffe <alexwokeeffe@gmail.com>
2018-08-10 09:38:03 +0100

    add video

--- wp-content/themes/eatup/page-templates/give-us-a-hand.php
+++ wp-content/themes/eatup/page-templates/give-us-a-hand.php
@@ -42,7 +42,7 @@ get_header() ?>
       </div>
       <div class="col-md-5 col-md-push-1 col-xs-12">
         <div class="video-container">
-          <iframe src="https://player.vimeo.com/video/280059054" style="position:absolute;top:0;left:0;width:100%;height:100%"; frameborder=“0” webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div><script src=“https://player.vimeo.com/api/player.js”></script>
+          <iframe src="https://player.vimeo.com/video/280059054?autoplay=1&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%"; frameborder=“0” webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div><script src=“https://player.vimeo.com/api/player.js”></script>
         </div>
       </div>
     </div>
